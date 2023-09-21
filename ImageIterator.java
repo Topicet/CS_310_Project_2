b61d7079a58e689d59734bf166ac617e1d8e8650
Nick <nick@ncofer.com>
2023-09-21 16:11:20 -0400

    Finished imageIterator

--- ImageIterator.java
+++ ImageIterator.java
@@ -1,16 +1,17 @@
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class ImageIterator<T extends Comparable<T>> implements Iterator<Node<T>>{
     private Node<T> currentNode;
     private Direction direction;
 
 
-    public ImageIterator(){
+    public ImageIterator(Node<T> head){
+        this.currentNode = head;
         this.direction = Direction.HORIZONTAL;
     }
 
-    public ImageIterator(Direction direction) {
+    public ImageIterator(Node<T> head, Direction direction) {
+        this.currentNode = head;
         this.direction = direction;
     }
     /**
@@ -22,11 +23,16 @@ public class ImageIterator<T extends Comparable<T>> implements Iterator<Node<T>>
 
     @Override
     public boolean hasNext() {
-        // Checks if there are more elements to consume
+        if (currentNode == null) {
+            return false;
+        }        
+        // For the Horizontal direction, check if there's a node to the right or if it can move down to the next row
         if (direction == Direction.HORIZONTAL) {
-            return (currentNode != null && currentNode.getRight() != null);
-        } else { // VERTICAL
-            return (currentNode != null && currentNode.getDown() != null);
+            return currentNode.getRight() != null || currentNode.getDown() != null;
+        } 
+        // For the vertical direction, check if there's a node below or if it can move right to the next column
+        else { 
+            return currentNode.getDown() != null || currentNode.getRight() != null;
         }
     }
 
@@ -34,19 +40,51 @@ public class ImageIterator<T extends Comparable<T>> implements Iterator<Node<T>>
     public Node<T> next() {
         // Fetches the next node in the direction specified
         if (!hasNext()) {
-            throw new NoSuchElementException("No more elements to iterate.");
+            return null;
         }
-
-        Node<T> toReturn = currentNode;
 
         if (direction == Direction.HORIZONTAL) {
-            // Move right for HORIZONTAL traversal
-            currentNode = currentNode.getRight();
-        } else { // VERTICAL
-            // Move down for VERTICAL traversal
-            currentNode = currentNode.getDown();
-        }
 
-        return toReturn;
+            // If there's a node to the right, move right
+            if (currentNode.getRight() != null) {
+                currentNode = currentNode.getRight();
+                return currentNode;
+            } 
+
+            // Otherwise, move to the first node of the next row
+            else if (currentNode.getDown() != null) {
+                currentNode = currentNode.getDown();
+
+                while (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                }            
+                return currentNode;
+            }
+            //If there is no node to right and no node below then we are at the end.
+            else{
+                return null;
+            }
+        } 
+        else { // VERTICAL
+            // If there's a node below, move down
+            if (currentNode.getDown() != null) {
+                currentNode = currentNode.getDown();
+                return currentNode;
+            } 
+            // Otherwise, move to the first node of the next column
+            else if (currentNode.getRight() != null) {
+                currentNode = currentNode.getRight();
+
+                while (currentNode.getUp() != null) {
+                    currentNode = currentNode.getUp();
+                }            
+                return currentNode;
+            }
+
+            //If there is no node to the right and no node below we are done.
+            else{
+                return null;
+            }
+        }   
     }
 }
